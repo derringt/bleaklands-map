01e38259a7dcb47e4d3bd3c4dd326a7d79014536
Drew Errington <drew@errington.xyz>
2019-06-02 18:57:39 -0400

    Changed Index Page to be multi Map

--- js/get-data.js
+++ js/get-data.js
@@ -3,3 +3,41 @@ function init() {
 						callback: callback,
 						simpleSheet: false } )
 }
+function callback(data, tabletop) {
+	addBadlands(data["Markers"].elements, tabletop);
+	addByaza(data["Byaza Markers"].elements, tabletop);
+	addSolitude(data["Solitude Markers"].elements, tabletop);
+	addMeridiem(data["Meridiem Markers"].elements, tabletop);
+}
+
+
+function addByaza(data, tabletop) {
+	for(i = 0; i < data.length; i++) {
+		if (data[i].Color) { color = data[i].Color.toLowerCase() + 'Icon'; } else { color = 'blackIcon'; }
+		L.marker([-data[i].Y,data[i].X], {icon: window[color]}).addTo(byazaMarkers).bindPopup('<b>'+data[i].Name+'</b><br>'+data[i].Description);
+	}
+}
+
+function addBadlands(data, tabletop) {
+	for(i = 0; i < data.length; i++) {
+		if (!data[i].Y || !data[i].X || !data[i].Name) { continue; }
+		
+		if (data[i].Color) { color = data[i].Color.toLowerCase() + 'Icon'; } else { color = 'blackIcon'; }
+		if (data[i].Group) { group = window[data[i].Group.toLowerCase()]; } else { group = map; }
+		L.marker([-data[i].Y,data[i].X], {icon: window[color]}).addTo(group).bindPopup('<b>'+data[i].Name+'</b><br>'+data[i].Description);
+	}
+}
+
+function addSolitude(data, tabletop) {
+	for(i = 0; i < data.length; i++) {
+		if (data[i].Color) { color = data[i].Color.toLowerCase() + 'Icon'; } else { color = 'blackIcon'; }
+		L.marker([-data[i].Y,data[i].X], {icon: window[color]}).addTo(solitudeMarkers).bindPopup('<b>'+data[i].Name+'</b><br>'+data[i].Description);
+	}
+}
+
+function addMeridiem(data, tabletop) {
+	for(i = 0; i < data.length; i++) {
+		if (data[i].Color) { color = data[i].Color.toLowerCase() + 'Icon'; } else { color = 'blackIcon'; }
+		L.marker([-data[i].Y,data[i].X], {icon: window[color]}).addTo(meridiemMarkers).bindPopup('<b>'+data[i].Name+'</b><br>'+data[i].Description);
+	}
+}
--- js/map-setup.js
+++ js/map-setup.js
@@ -4,13 +4,13 @@ var map = L.map('mapid', {
 	zoomDelta: 0.25,
 	minZoom: 2,
 	maxZoom: 5
-});
+	});
 
 //Change the URL to reflect where you are hosting your map tiles. Width and Height of original image MUST be defined.
 var layer = L.tileLayer.zoomify('./badlands-map/{g}/{z}-{x}-{y}.jpg', {
     width: 4096,    // MUST be defined.
     height: 3072,   // MUST be defined.
-}).addTo(map);
+	}).addTo(map);
 
 //Setting the view to our layer bounds, set by our Zoomify plugin.
 map.fitBounds(layer.getBounds());
@@ -20,10 +20,21 @@ var badlands = L.tileLayer.zoomify('./badlands-map/{g}/{z}-{x}-{y}.jpg', {
     width: 4096,                                                                                        // MUST be defined.
     height: 3072,                                                                                       // MUST be defined.
     tolerance: 0.9, 
-    }).addTo(map);
+	}).addTo(map);
 	
-//Creates the switchable map layers. Change the URL to reflect where you are hosting your map tiles. Width and Height of original image MUST be defined.
 var byaza = L.tileLayer.zoomify('./byaza-map/{g}/{z}-{x}-{y}.jpg', {
+    width: 2048,                                                                                        // MUST be defined.
+    height: 1536,                                                                                       // MUST be defined.
+    tolerance: 0.9, 
+	});
+
+var solitude = L.tileLayer.zoomify('./solitude-map/{g}/{z}-{x}-{y}.jpg', {
+    width: 2048,                                                                                        // MUST be defined.
+    height: 1536,                                                                                       // MUST be defined.
+    tolerance: 0.9, 
+	});
+	
+var meridiem = L.tileLayer.zoomify('./meridiem-map/{g}/{z}-{x}-{y}.jpg', {
     width: 2048,                                                                                        // MUST be defined.
     height: 1536,                                                                                       // MUST be defined.
     tolerance: 0.9, 
@@ -31,18 +42,22 @@ var byaza = L.tileLayer.zoomify('./byaza-map/{g}/{z}-{x}-{y}.jpg', {
 
 // Creates baseMaps layer and passes which maps to include in the layers control.
 var baseMaps = {
-    "Map of the Badlands": badlands,
-	"Map of Byaza": byaza,
-};
+    "The Badlands": badlands,
+	"Byaza": byaza,
+	"Meridiem": meridiem,
+	"Solitude": solitude,
+	};
 
 var poi = L.layerGroup();
 var towns = L.layerGroup();
 var byazaMarkers = L.layerGroup();
+var solitudeMarkers = L.layerGroup();
+var meridiemMarkers = L.layerGroup();
 
 map.addLayer(poi);
 map.addLayer(towns);
 
-var allLayers = { poi, towns, byazaMarkers };
+var allLayers = { poi, towns, byazaMarkers, solitudeMarkers, meridiemMarkers };
 
 var badlandsMarkers = {
 	"Towns" : towns,
@@ -58,16 +73,45 @@ map.on('baselayerchange', function(e) {
 		control.removeLayer(allLayers[layerGroup]);
 	}
 	switch(e.name) {
-		case 'Map of Byaza':
+		case 'Byaza':
 			byazaMarkers.addTo(map);
-			console.log('Byaza');
 			break;
-		case 'Map of the Badlands':
+		case 'The Badlands':
 			poi.addTo(map);
 			towns.addTo(map);
 			control.addOverlay(towns, "Towns");
 			control.addOverlay(poi, "Points of Interest");
 			break;
+		case 'Solitude':
+			solitudeMarkers.addTo(map);
+			break;
+		case 'Meridiem':
+			meridiemMarkers.addTo(map);
+			break;
 		default:
 	}
 });
+
+function swapMap(name) {
+	var layerControlElement = document.getElementsByClassName('leaflet-control-layers')[0];
+	switch(name) {
+		case 'Badlands':
+			layerControlElement.getElementsByTagName('input')[0].click();
+			return false;
+			break;
+		case 'Byaza':
+			layerControlElement.getElementsByTagName('input')[1].click();
+			return false;
+			break;
+		case 'Meridiem':
+			layerControlElement.getElementsByTagName('input')[2].click();
+			return false;
+			break;
+		case 'Solitude':
+			layerControlElement.getElementsByTagName('input')[3].click();
+			return false;
+			break;			
+		default:
+			return true;
+	}
+}
