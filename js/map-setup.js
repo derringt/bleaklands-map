917a355bdaf0021555f073e49973b6685825fdbc
Drew Errington <drew@errington.xyz>
2021-12-01 00:31:22 -0500

    Swap the map

--- js/map-setup.js
+++ js/map-setup.js
@@ -2,29 +2,29 @@
 var map = L.map('mapid', { 
     crs: L.CRS.Simple, //Set a flat CRS (Coordinate Reference System) projection as we are projecting an image.
 	zoomDelta: 0.25,
-	minZoom: 2.25,
+	minZoom: 2.5,
 	maxZoom: 5
 	});
 	
 //Change the URL to reflect where you are hosting your map tiles. Width and Height of original image MUST be defined.
-var layer = L.tileLayer.zoomify('./nevrisea-map/{g}/{z}-{x}-{y}.jpg', {
-    width: 4000,    // MUST be defined.
-    height: 2000,   // MUST be defined.
+var layer = L.tileLayer.zoomify('./the-bleak-lands-map/{g}/{z}-{x}-{y}.jpg', {
+    width: 3200,    // MUST be defined.
+    height: 1800,   // MUST be defined.
 	}).addTo(map);
 
 //Setting the view to our layer bounds, set by our Zoomify plugin.
 map.fitBounds(layer.getBounds());
 
 //Creates the switchable map layers. Change the URL to reflect where you are hosting your map tiles. Width and Height of original image MUST be defined.
-var nevrisea = L.tileLayer.zoomify('./nevrisea-map/{g}/{z}-{x}-{y}.jpg', {
-    width: 4000,                                                                                        // MUST be defined.
-    height: 2000,                                                                                       // MUST be defined.
+var bleaklands = L.tileLayer.zoomify('./the-bleak-lands-map/{g}/{z}-{x}-{y}.jpg', {
+    width: 3200,                                                                                        // MUST be defined.
+    height: 1800,                                                                                       // MUST be defined.
     tolerance: 0.9, 
 	}).addTo(map);
 
 // Creates baseMaps layer and passes which maps to include in the layers control.
 var baseMaps = {
-    "Nevrisea": nevrisea,
+    "The Bleak Lands": bleaklands
 	};
 
 var poi = L.layerGroup();
@@ -35,7 +35,7 @@ map.addLayer(towns);
 
 var allLayers = { poi, towns };
 
-var nevriseaMarkers = {
+var bleaklandsMarkers = {
 	"Towns" : towns,
 	"Points of Interest": poi,
 	};
@@ -49,7 +49,7 @@ map.on('baselayerchange', function(e) {
 		control.removeLayer(allLayers[layerGroup]);
 	}
 	switch(e.name) {
-		case 'Nevrisea':
+		case 'The Bleak Lands':
 			poi.addTo(map);
 			towns.addTo(map);
 			control.addOverlay(towns, "Towns");
@@ -62,7 +62,7 @@ map.on('baselayerchange', function(e) {
 function swapMap(name) {
 	var layerControlElement = document.getElementsByClassName('leaflet-control-layers')[0];
 	switch(name) {
-		case 'Nevrisea':
+		case 'The Bleak Lands':
 			layerControlElement.getElementsByTagName('input')[0].click();
 			return false;
 			break;	
